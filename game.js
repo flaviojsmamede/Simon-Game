a08084e0016f044272e7a788e294c38743ae0246
Flávio <85414870+flaviojsmamede@users.noreply.github.com>
2022-03-07 10:42:28 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -5,9 +6,20 @@ var gamePattern = [];
 function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
-
   var randomChosenColour = buttonColours[randomNumber];
-
   gamePattern.push(randomChosenColour);
+
+  // if (randomChosenColour === "red") {
+    $("#red").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).on("animationend", function() {
+      var audio = new Audio('sounds/red.mp3');
+      audio.play();
+    });
+  } else if (randomChosenColour === "blue") {
+    $("#blue").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  } else if (randomChosenColour === "green") {
+    $("#green").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  } else {
+    $("#yellow").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  };
 
 };
