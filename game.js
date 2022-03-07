9434b114064b8fe9aabb8fc2dcbc06eb5f7b5d26
Flávio <85414870+flaviojsmamede@users.noreply.github.com>
2022-03-07 10:58:16 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -1,25 +1,22 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
 function nextSequence() {
-
+//Selecting a rnndom number
   var randomNumber = Math.floor(Math.random() * 4);
+
+//Selecting one colour inside the array
   var randomChosenColour = buttonColours[randomNumber];
+
+//Putting the selected colour inside an array
   gamePattern.push(randomChosenColour);
 
-  // if (randomChosenColour === "red") {
-    $("#red").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).on("animationend", function() {
-      var audio = new Audio('sounds/red.mp3');
-      audio.play();
-    });
-  } else if (randomChosenColour === "blue") {
-    $("#blue").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  } else if (randomChosenColour === "green") {
-    $("#green").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  } else {
-    $("#yellow").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  };
+//Making an animation for the selected button
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+//Playing an audio for the selected button
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 
 };
